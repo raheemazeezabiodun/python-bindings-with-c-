d381d3a6698b0490b84f3dd3a55eb90cd93def3a
Raheem Azeez Abiodun <raheemazeez4@gmail.com>
2020-03-09 10:38:24 +0100

    Playground for ctypes examples, written c++ functions that needs to be called

---
+++ playground/c++/example.cpp
@@ -0,0 +1,8 @@
+#include "example.hpp"
+
+float divide_numbers(int x, int y) {
+    float result = x / y;
+    cout << "Dividing " << x << "by " << y << "gives " << result;
+    
+    return result;
+}
---
+++ playground/c++/example.hpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+using std::cout;
+
+float divide_numbers(int x, float y);
